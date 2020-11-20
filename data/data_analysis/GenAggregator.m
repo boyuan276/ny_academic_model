e38912364df6c60002ec4dd499973bad901fe29c
Bo Yuan <by276@cornell.edu>
2020-11-19 20:05:25 -0500

    recalculate min gen and cost curve

--- data/data_analysis/GenAggregator.m
+++ data/data_analysis/GenAggregator.m
@@ -5,12 +5,16 @@ function [P_max,P_min, RampUp_max, RampDown_max] = GenAggregator(generators)
 %   generators associate to one of the eight thermal generators in NYAM.
 
 gen_hourly = groupsummary(generators, ["Date", "Hour"], "sum", "GrossLoadMW");
+gen_hourly = gen_hourly(gen_hourly.sum_GrossLoadMW > 0, :);
 P_max = max(gen_hourly.sum_GrossLoadMW);
-P_min = min(gen_hourly.sum_GrossLoadMW);
+lowPercent = 5;
+highPercent = 95;
+[~,P_min,~,~] = isoutlier(gen_hourly.sum_GrossLoadMW,"percentiles", [lowPercent, highPercent]);
 ramp_rate = gen_hourly.sum_GrossLoadMW(2:end) - gen_hourly.sum_GrossLoadMW(1:end-1);
 ramp_rate = [ramp_rate; 0];
 gen_hourly.RampRate = ramp_rate;
-RampUp_max = max(gen_hourly.RampRate);
-RampDown_max = min(gen_hourly.RampRate);
+[~,RampDown_max,RampUp_max,~] = isoutlier(gen_hourly.RampRate,"percentiles", [1, 99]);
+% RampUp_max = max(gen_hourly.RampRate);
+% RampDown_max = min(gen_hourly.RampRate);
 end
 
